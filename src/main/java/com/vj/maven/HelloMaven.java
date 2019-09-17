2cb46fad73a043c321cccd84625b1324cd90454c
User <vj.ku.99@gmail.com>
2019-09-17 11:23:47 +0530

    junit 4 and 5 both run in single project

--- src/main/java/com/vj/maven/HelloMaven.java
+++ src/main/java/com/vj/maven/HelloMaven.java
@@ -3,10 +3,12 @@ package com.vj.maven;
 public class HelloMaven {
 
     public static void main(String[] args) {
+
+
         System.out.println("Hello Maven !");
     }
 
-    public String getHello(){
+    public String getHello() {
         return "Hello World";
     }
 }
