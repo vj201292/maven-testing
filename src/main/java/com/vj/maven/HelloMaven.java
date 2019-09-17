e9660cc8fb0f818bbebb2d7253a6b2701b737bdb
User <vj.ku.99@gmail.com>
2019-09-17 11:49:26 +0530

    added groovy dependency

--- src/main/java/com/vj/maven/HelloMaven.java
+++ src/main/java/com/vj/maven/HelloMaven.java
@@ -5,4 +5,8 @@ public class HelloMaven {
     public static void main(String[] args) {
         System.out.println("Hello Maven !");
     }
+
+    public String Hello(){
+        return "Hello";
+    }
 }
---
+++ src/test/java/com/vj/maven/HelloMavenTest.java
@@ -0,0 +1,12 @@
+package com.vj.maven;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloMavenTest {
+
+    @Test
+    public void hello() {
+    }
+}
